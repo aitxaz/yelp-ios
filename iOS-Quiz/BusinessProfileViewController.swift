e324911dd558576bdc18357a94f2e522b22e6c68
Mohsin Khan <mohsin.khan@rcapps.net>
2017-05-19 19:44:15 +0500

    Initial commit

---
+++ iOS-Quiz/BusinessProfileViewController.swift
@@ -0,0 +1,9 @@
+//
+//  BusinessProfileViewController.swift
+//  iOS-Quiz
+//
+//  Created by Muhammad Aitizaz Khalid on 5/19/17.
+//  Copyright © 2017 RCAPPS. All rights reserved.
+//
+
+import Foundation
---
+++ iOS-Quiz/MapViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  iOS-Quiz
+//
+//  Created by Muhammad Aitizaz Khalid on 5/19/17.
+//  Copyright © 2017 RCAPPS. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
